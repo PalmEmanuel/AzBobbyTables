5e1dd7ce7e9b8ddab48fe5f105513b39c1481af2
Emanuel Palm <ep__palm@hotmail.com>
2021-06-21 19:28:27 +0200

    dotnet standard 2.1 again, working command

--- Source/AzBobbyTables/Cmdlets/AddAzDataTableRow.cs
+++ Source/AzBobbyTables/Cmdlets/AddAzDataTableRow.cs
@@ -7,7 +7,7 @@ using System.Management.Automation;
 namespace PipeHow.AzBobbyTables.Cmdlets
 {
     /// <summary>
-    /// 
+    /// Add a row to an Azure Table.
     /// </summary>
     [Cmdlet(VerbsCommon.Add, "AzDataTableRow")]
     public class AddAzDataTableRow : Cmdlet
@@ -16,25 +16,26 @@ namespace PipeHow.AzBobbyTables.Cmdlets
         /// <para type="description">The name of the table.</para>
         /// </summary>
         [Parameter(Mandatory = true)]
-        public string ConnectionS { get; set; }
+        [ValidateNotNullOrEmpty()]
+        public string ConnectionString { get; set; }
 
         /// <summary>
         /// <para type="description">The name of the table.</para>
         /// </summary>
         [Parameter(Mandatory = true)]
+        [ValidateNotNullOrEmpty()]
         public string TableName { get; set; }
 
         /// <summary>
         /// <para type="description">The entries to add to the table.</para>
         /// </summary>
         [Parameter(Mandatory = true)]
+        [ValidateNotNullOrEmpty()]
         public Hashtable Row { get; set; }
 
-        public AddAzDataTableRow()
+        protected override void ProcessRecord()
         {
-            base.ProcessRecord();
-
-            AzDataTableService.Connect(ConnectionS, TableName);
+            AzDataTableService.Connect(ConnectionString, TableName);
 
             AzDataTableService.AddRowToTable(Row);
         }

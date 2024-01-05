56615b0d2a8298dcc0c2df0acd3392cdde93b8d7
Emanuel Palm <emanuel+github@pipe.how>
2024-01-05 22:05:29 +0100

    initial sampler setup

---
+++ .build/tasks/PlatyPS.RepairPlatyPSMarkdown.ps1
@@ -0,0 +1,5 @@
+task repairPlatyPSHelp {
+    # Workaround to run post-build to avoid platyPS generating documentation for common parameter ProgressAction
+    . "$BuildRoot\Scripts\PlatyPSWorkaround.ps1"
+    Repair-PlatyPSMarkdown -Path (Get-ChildItem "$BuildRoot\Docs\Help") -ParameterName 'ProgressAction'
+}

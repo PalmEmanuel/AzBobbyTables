c4658ab303b8d35d1c6836259bc78b7163b0ad52
Emanuel Palm <emanuel+github@pipe.how>
2024-01-06 16:45:00 +0100

    lowercase folders and paths

--- .build/tasks/PlatyPS.UpdateMarkdownHelp.ps1
+++ .build/tasks/PlatyPS.UpdateMarkdownHelp.ps1
@@ -2,5 +2,5 @@ task updateMarkdownHelp {
     Import-Module 'platyPS' -ErrorAction 'Stop'
     $ProjectName = Get-SamplerProjectName -BuildRoot $BuildRoot
     Import-Module "$BuildModuleOutput/$ProjectName" -Force -ErrorAction 'Stop'
-    Update-MarkdownHelpModule -Path "$BuildRoot/$($BuildInfo.PlatyPS.HelpMarkdownFolder)"
+    Update-MarkdownHelpModule -Path "$BuildRoot/$HelpSourceFolder/$HelpOutputFolder"
 }

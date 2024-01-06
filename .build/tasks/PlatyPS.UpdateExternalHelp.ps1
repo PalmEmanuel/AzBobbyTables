789256e7860de458758c564033214bb6b6f5e78b
Emanuel Palm <emanuel+github@pipe.how>
2024-01-06 16:13:17 +0100

    capitalization

--- .build/tasks/PlatyPS.UpdateExternalHelp.ps1
+++ .build/tasks/PlatyPS.UpdateExternalHelp.ps1
@@ -3,5 +3,5 @@ task updateExternalHelp {
     Import-Module 'platyPS' -ErrorAction 'Stop'
     $ModuleManifestPath = (Get-Module -ListAvailable "$BuildModuleOutput/$ProjectName" | Sort-Object Version -Descending | Select-Object -First 1).Path
     $ModuleVersionPath = Split-Path -Path $ModuleManifestPath -Parent
-    New-ExternalHelp -Path "$BuildRoot/$HelpSourceFolder/$HelpOutputFolder/" -OutputPath "$ModuleVersionPath/$HelpCultureInfo/" -Force
+    New-ExternalHelp -Path "$BuildRoot/Docs/Help/" -OutputPath "$ModuleVersionPath/$HelpCultureInfo/" -Force
 }
--- .build/tasks/PlatyPS.UpdateMarkdownHelp.ps1
+++ .build/tasks/PlatyPS.UpdateMarkdownHelp.ps1
@@ -2,5 +2,5 @@ task updateMarkdownHelp {
     Import-Module 'platyPS' -ErrorAction 'Stop'
     $ProjectName = Get-SamplerProjectName -BuildRoot $BuildRoot
     Import-Module "$BuildModuleOutput/$ProjectName" -Force -ErrorAction 'Stop'
-    Update-MarkdownHelpModule -Path "$BuildRoot/$HelpSourceFolder/$HelpOutputFolder/"
+    Update-MarkdownHelpModule -Path "$BuildRoot/Docs/Help/"
 }
